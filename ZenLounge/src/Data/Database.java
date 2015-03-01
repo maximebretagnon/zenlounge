9bef703b9a02d5dfb726a1c204732e29f0ee3b19
maximebretagnon <Maxime@PC>
2015-03-01 12:56:55 +0100

    Database class

--- ZenLounge/src/Data/Database.java
+++ ZenLounge/src/Data/Database.java
@@ -1,28 +1,68 @@
 package Data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class Database
-{
-	private static boolean dbOpened;
-		
-    private static final String login = "Database Login";
-    private static final String pwd = "Database Password";
-    
+{	
+	private static final String url = "jdbc:mysql://pandacoding.c0iuibsqekzw.us-west-2.rds.amazonaws.com:3306/ZenLounge";
+	private static final String login = "pandacoding";
+    private static final String pwd = "pandacoding";
+    private static Connection connexion = null;
     
     public static void open() {
-    	//Open the db
-    	dbOpened = true;
+		
+    	/* Chargement du driver */
+		try {
+		    Class.forName( "com.mysql.jdbc.Driver" );
+		} catch ( ClassNotFoundException e ) {
+		    /* Gérer les éventuelles erreurs ici. */
+		}
+		
+		/* Ouverture de la connexion */
+    	try {
+		    connexion = DriverManager.getConnection( url, login, pwd );
+		} catch ( SQLException e ) {
+		    /* Gérer les éventuelles erreurs ici */
+		}
     }
 
-    public static Object request(String request) {
-    	if(!dbOpened)
+    public static ResultSet request(String request) {
+    	
+    	/* Si la connexion n'existe pas, on l'ouvre */
+    	if (connexion == null)
     		open();
-    	//Execute request
-    	return null;
+    	
+    	/* Exécution d'une requête de lecture */
+    	ResultSet resultat = null;
+    	Statement statement = null;
+		
+    	try {
+			statement = connexion.createStatement();
+		} catch (SQLException e1) {
+			/* Gérer les éventuelles erreurs ici */
+		}
+    	
+    	try {
+			resultat = statement.executeQuery( "SELECT id, email, mot_de_passe, nom  FROM Utilisateur;" );
+		} catch (SQLException e) {
+			/* Gérer les éventuelles erreurs ici */
+		}
+    	
+    	return resultat;
     }
 
     public static void close() {
-    	if(dbOpened)
-    		/*Close the db*/;
-    	dbOpened = false;
+	    if (connexion != null)
+	        try {
+	            /* Fermeture de la connexion */
+	            connexion.close();
+	        } catch ( SQLException ignore ) {
+	            /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+	        }
     }
 }
--- ZenLounge/src/Main.java
+++ ZenLounge/src/Main.java
@@ -2,14 +2,10 @@ import UI.Common.Frame;
 import UI.Views.LoginView;
 
 public class Main {
-
-	public static void main(String[] args) {
-			
-		LoginView loginView = new LoginView();
-		
-		Frame f = Frame.getFrame();
-		f.setView(loginView);
-		f.setVisible(true);
-		
+	public static void main(String[] args) {	
+			LoginView loginView = new LoginView();
+			Frame f = Frame.getFrame();
+			f.setView(loginView);
+			f.setVisible(true);
 	}
 }
